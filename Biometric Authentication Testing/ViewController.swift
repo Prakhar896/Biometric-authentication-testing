e803754aa47ca403d0c3ec277816d0684c0ac806
Prakhar Trivedi <prakhar0706@gmail.com>
2020-03-25 18:50:27 +0800

    Initial Commit

---
+++ Biometric Authentication Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Biometric Authentication Testing
+//
+//  Created by Prakhar Trivedi on 25/3/20.
+//  Copyright © 2020 Prakhar Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Biometric Authentication TestingTests/Biometric_Authentication_TestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Biometric_Authentication_TestingTests.swift
+//  Biometric Authentication TestingTests
+//
+//  Created by Prakhar Trivedi on 25/3/20.
+//  Copyright © 2020 Prakhar Trivedi. All rights reserved.
+//
+
+import XCTest
+@testable import Biometric_Authentication_Testing
+
+class Biometric_Authentication_TestingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
